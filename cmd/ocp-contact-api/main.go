139bbc940dd81a5ddf26beea678a195de9d34e05
echekunov <echekunov@gmail.com>
2021-07-24 03:03:32 +0300

    - Opened/closed file in the loop with defer - added type Contact - refactored splitting on the batches routine to support Contact type - added slice convert function to the map

--- cmd/ocp-contact-api/main.go
+++ cmd/ocp-contact-api/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
 	fmt.Println("This is an Ozon Contact API")
+	const configPath string = "config.cfg"
+
+	fmt.Printf("Creating a file %v\n", configPath)
+	file, err := os.Create(configPath)
+	if err != nil {
+		fmt.Printf("Cannot create file by path %v, error %v\n", configPath, err)
+		return
+	}
+
+	file.WriteString("config file content")
+	file.Close()
+
+	readConfig := func() {
+		file, err := os.Open(configPath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("File %v was opened\n", configPath)
+		defer func() {
+			if err := file.Close(); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("File %v was closed\n", configPath)
+			}
+		}()
+		bytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf(" Read content: %v\n", string(bytes))
+		}
+	}
+
+	for i := 0; i < 10; i++{
+		readConfig()
+	}
+
+	defer func(){
+		if err := os.Remove(configPath); err == nil {
+			fmt.Printf("File %v was removed\n", configPath)
+		} else {
+			fmt.Printf("File %v wasn't removed, error %v\n", configPath, err)
+		}
+	}()
 }
